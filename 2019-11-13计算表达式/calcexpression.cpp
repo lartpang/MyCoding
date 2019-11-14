a7305410067094210dd0627ed2a010198a1b0577
MY_ <lartpang@163.com>
2019-11-14 18:23:23 +0800

    Update calcexpression.cpp

--- 2019-11-13计算表达式/calcexpression.cpp
+++ 2019-11-13计算表达式/calcexpression.cpp
@@ -15,6 +15,8 @@ using namespace std;
 
 class ExpressionChecker {
 private:
+    string modified_str;
+
     static bool IsChar(char item) {
         // 可以是连续的英文字符
         return (item >= 'a' && item <= 'z') || (item >= 'A' && item <= 'Z');
@@ -28,6 +30,10 @@ private:
         return item == '+' || item == '-' || item == '*' || item == '/';
     };
 
+    static bool IsPair(char item) {
+        return item == '(' || item == ')';
+    };
+
     static bool IsNum(char item) {
         // 只判断单个数字
         return item >= '0' && item <= '9';
@@ -38,6 +44,10 @@ private:
     static vector<string> GetNumRelationship(const string &in_string);
 
 public:
+    void SetPrivateVar(string in_string) { modified_str = std::move(in_string); };
+
+    string GetPrivateVar() { return modified_str; };
+
     // todo: 完善更多计算模式的支持
     ExpressionChecker() {
         cout << "Init the Checker Class with default mode" << endl;
@@ -51,15 +61,15 @@ public:
     static bool FilterWhitespace(string &in_string);
 
     // 检查括号匹配问题以及字符合法性，只能有括号和数字，字符，运算符（+,-,*,/)
-    static bool CharChecker(const string &in_string);
+    static bool IsCharValid(const string &in_string);
 
     // 检查操作符是否正常，并调整--为+
-    static bool OperatorChecker(string in_string);
+    static bool IsOperatorValid(string in_string);
 
-    static bool DotChecker(string in_string);
+    static bool IsFloatValid(vector<string> in_string);
 
 
-    static vector<string> NegativeChecker(string in_string);
+    vector<string> NegativeChecker(string in_string);
 
     /*
     1. 依次读取字符串字符，转化为后缀表达式
@@ -70,6 +80,9 @@ public:
     static bool CalcExpression(const vector<string> &in_string, double &result);
 };
 
+/*
+ * 将数字字符串转化为double浮点数
+ * */
 double ExpressionChecker::String2Number(string in_string) {
     // f1.001 -> 1.001 -> 1001 -> 100.1 point_place=1, i-point_place=i-1=>-1,0,1,2
 
@@ -82,7 +95,6 @@ double ExpressionChecker::String2Number(string in_string) {
     }
     reverse(in_string.begin(), in_string.end());
 
-    // todo: 需要检查是否仅有一个点号
     for (int i = 0; i < in_string.size(); ++i) {
         if (in_string[i] == '.') {
             point_place = i;
@@ -98,8 +110,9 @@ double ExpressionChecker::String2Number(string in_string) {
     return item;
 }
 
-
-// 根据后缀表达式转换原始数据
+/*
+ * 根据后缀表达式转换原始数据，对原始数据中的非运算符部分进行划分
+ * */
 vector<string> ExpressionChecker::GetNumRelationship(const string &in_string) {
     vector<string> char_vec;
     string item_str;
@@ -119,51 +132,86 @@ vector<string> ExpressionChecker::GetNumRelationship(const string &in_string) {
 
 /*
  * 对于字符串中的特定符号进行滤除
- * 1. 空白符
- * 2. 类型符号：f
- * 3. 只保留第一个分号之前的内容，没有分号的话，完全读取
+ * 1. 空白符或者空白行
+ * 2. 只保留第一个分号之前的内容，没有分号的话，完全读取
+ * 3. 跳过//为首的注释行，前后可以存在空白符
  * */
 bool ExpressionChecker::FilterWhitespace(string &in_string) {
     string new_string;
     for (char i : in_string) {
-        if (!(i == '\t' || i == '\r' || i == '\n')) {
+        if ((i != ' ' && i != '\t' && i != '\r' && i != '\n')) {
             if (i == ';') {
                 break;
             }
             new_string.push_back(i);
         }
     }
+
+    // 跳过注释行
+    if (new_string[0] == '/' && new_string[1] == '/') {
+        return false;
+    }
+
     in_string = new_string;
 
     return !new_string.empty();
 }
 
-bool ExpressionChecker::CharChecker(const string &in_string) {
-    stack<char> bracket_stack;
+/*
+ * 检查字符串中的字符合法问题以及括号匹配问题
+ * */
+bool ExpressionChecker::IsCharValid(const string &in_string) {
+    int count = 0;
 
     // 检查符号合法问题
     for (char i : in_string) {
         // 不满足合理范围
-        if (!(IsOperator(i) || IsNum(i) || IsChar(i) || IsDot(i))) {
+        if (!(IsOperator(i) || IsNum(i) || IsChar(i) || IsDot(i) || IsPair(i))) {
             return false;
         }
     }
+
     // 检查括号匹配问题
     for (char i : in_string) {
         if (i == '(') {
-            bracket_stack.push('(');
+            count++;
         } else if (i == ')') {
-            bracket_stack.pop();
+            count--;
         }
     }
 
-    return bracket_stack.empty();
+    return count == 0;
 }
 
-// 只允许有连续的-号，即--
-bool ExpressionChecker::OperatorChecker(string in_string) {
-    string positive_str;
+/*
+ * 检查字符串中的运算符合法问题
+ * 主要原则：不允许有连续符号，符号左侧可以是括号/字母/数字，右侧仅能是数字/字母/左括号
+ * */
+bool ExpressionChecker::IsOperatorValid(string in_string) {
+    string temp_str = std::move(in_string);
 
+    // 不论如何，首先插0，方便迭代
+    temp_str.insert(0, 1, '0');
+
+    // 每一个运算符右端必然是数字/字母/左括号，必然存在；
+    for (int i = 0; i < temp_str.size() - 1; ++i) {
+        if (IsOperator(temp_str[i])) {
+            if (!(IsNum(temp_str[i + 1]) || IsChar(temp_str[i + 1]) || temp_str[i + 1] == '(')) {
+                return false;
+            }
+        }
+    }
+
+    // 左端可以为括号/字母/数字
+    for (int i = 1; i < temp_str.size(); ++i) {
+        if (IsOperator(temp_str[i])) {
+            if (!(IsNum(temp_str[i - 1]) || IsChar(temp_str[i - 1]) || IsPair(temp_str[i - 1]))) {
+                return false;
+            }
+        }
+    }
+
+    /*
     // 开头不允许有两个操作符，结尾不允许有操作符
     if (IsOperator(in_string[0]) && IsOperator(in_string[1])) {
         return false;
@@ -199,27 +247,74 @@ bool ExpressionChecker::OperatorChecker(string in_string) {
             }
         }
     }
-
-    in_string = positive_str;
-
+     */
     return true;
 }
 
-bool ExpressionChecker::DotChecker(string in_string) {
-    return in_string.find('.') == in_string.rfind('.');
+// 点号的检查，点的左右必须是数字
+bool ExpressionChecker::IsFloatValid(vector<string> in_string) {
+    int pos;
+
+    for (string item:in_string) {
+        bool has_last_f = item[item.size() - 1] == 'f';
+        int num_num = 0, num_char = 0;
+
+        // 检查计算项不允许数字与字母混用，仅能在数字后使用f
+        for (char i_char : item) {
+            // 此处仅剩数字，字符，点号，运算符
+            if (IsOperator(i_char) || IsDot(i_char) || IsPair(i_char)) {
+                continue;
+            }
+            // 数字，字符数量统计
+            if (IsNum(i_char)) {
+                num_num++;
+            } else {
+                num_char++;
+            }
+        }
+
+        // 仅当只有数字或者只有字符的时候，以及数字和一个结尾的f的时候，才需要进一步检测，否则就是错误的
+        if (!((num_num > 0 && num_char == 1 && has_last_f) || num_num == 0 || num_char == 0)) {
+            return false;
+        }
+
+        // 开始检查小数情况下的合法性：对每项中的点号检查
+        if (item.find('.') == item.rfind('.')) {
+            // 只有一个.或者没有点
+            if (item.find('.') != string::npos) {
+                // 找到一个.
+                pos = item.find('.');
+                if (pos == 0 || pos == item.size() - 1) {
+                    return false;
+                } else {
+                    return IsNum(item[pos - 1]) && IsNum(item[pos + 1]);
+                }
+            }
+            // 没有点
+        } else {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 // 向负号前插0来保证都是正数，需要提前检查连续符号的合理性(使用过程中有连续符号必须加括号)
 vector<string> ExpressionChecker::NegativeChecker(string in_string) {
     // -a+b和b+(-a)这样的
-    if (in_string[0] == '-') {
-        in_string.insert(0, 1, '0');
+    for (int i = 0; i < in_string.size() - 1; ++i) {
+        if (in_string[i] == '-') {
+            if (i == 0 || in_string[i - 1] == '(') {
+                in_string.insert(i, 1, '0');
+            }
+        }
     }
 
+    SetPrivateVar(in_string);
     return GetNumRelationship(in_string);
 }
 
-/*
+/* 中缀转换后缀表达式：
  * 当读到操作数时，立即把它输出，即成为后缀表达式的一部分；
  * 若读到操作符，判断该符号与栈顶符号的优先级:
  *      若该符号优先级高于栈顶元素，则将该操作符入栈，
@@ -363,28 +458,28 @@ int main() {
             if (!checker.FilterWhitespace(linebuf)) {
                 continue;
             }
-            if (!checker.OperatorChecker(linebuf)) {
-                cout << "表达式非法" << endl;
+            if (!checker.IsCharValid(linebuf)) {
+                cout << "IsCharValid 表达式非法" << endl;
                 continue;
             }
-            if (!checker.CharChecker(linebuf)) {
-                cout << "表达式非法" << endl;
+            if (!checker.IsOperatorValid(linebuf)) {
+                cout << "IsOperatorValid 表达式非法" << endl;
                 continue;
             }
-            if (!checker.DotChecker(linebuf)) {
-                cout << "表达式非法" << endl;
-                continue;
-            }
-
 
             item_vec = checker.NegativeChecker(linebuf);
-            checker.TransPostfixExpression(item_vec);
+            if (!checker.IsFloatValid(item_vec)) {
+                cout << "IsFloatValid 表达式非法" << endl;
+                continue;
+            }
 
+            checker.TransPostfixExpression(item_vec);
             if (checker.CalcExpression(item_vec, result)) {
                 cout << "输出结果：" << result << endl;
             } else {
                 cout << "表达式合法" << endl;
             }
+            cout << " ==>> 获得的表达式：" << checker.GetPrivateVar() << endl;
         }
     } else  // 文件打开失败
     {
